90641bae0d3dce65c8248b4d99bed85086c92915
PuttipongMax <maxnetoogo@gmail.com>
2024-05-30 11:29:33 +0700

    feat: Create-Controller Create-form Form-validate Custom-form-validate

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,16 +1,31 @@
 @extends('layout')
 @section('title', 'บทความทั้งหมด')
 @section('content')
-  <h2>บทความทั้งหมด</h2>
-  <hr>
-  @foreach($blogs as $item)
-    <h4>{{$item["title"]}}</h4>
-    <p>{{$item["content"]}}</p> 
-    @if ($item["status"]==true)
-      <p class="text text-success">publish</p>
-    @else
-      <p class="text text-danger">draf</p>
-    @endif
-    <hr>
-  @endforeach
+  <h2 class="text text-center py-2">บทความทั้งหมด</h2>
+  <table class="table table-bordered text-center">
+    <thead>
+      <tr>
+        <th scope="col">title</th>
+        <th scope="col">content</th>
+        <th scope="col">status</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($blogs as $item)
+      <tr>
+        <td>{{$item["title"]}}</td>
+        <td>{{$item["content"]}}</td> 
+        <td>  
+          @if ($item["status"]==true)
+            <a href="#" class="btn btn-success">publish</a>
+          @else
+            <a href="#" class="btn btn-warning">draf</a>
+          @endif
+        </td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+ 
+  
 @endsection
